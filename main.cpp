bd9badee75c9cd7a99997961796f6e864e7b2331
Arma Burton <arma@armaburton.com>
2023-08-19 14:16:09 -0700

    updates

--- main.cpp
+++ main.cpp
@@ -1,3 +1,15 @@
+// NOTE: This template is to be used for partner practice ONLY! You must
+// use the required Algorithmic Design Document for all Assignments.
+/******************************************************************************
+    # Author:           Arma Burton
+    # Assignment:       002 Encoder
+    # Date:             July 9, 2023
+    # Description:      This program inputs values from the user and computes
+                        the cost and tip.
+    # Input:            int, char, double, string
+    # Output:           string, int, double
+    # Sources:          user input
+#******************************************************************************/
 #include <iostream>
 #include <cctype>
 #include <cstdlib>
@@ -19,6 +31,16 @@ void readInput(char fName[], char lName[], bool &lateFlag);
 void readInput(char parsedId[], char fileName[]);
 void readTime(char strTime[]);
 
+/*
+    Name:   main()
+    Desc:   This function reads menu input from the user
+            If the menu option is e then it calls the encode function
+                and passes the string encodedFileName
+            If the menu option is q then it quits the program
+    input:  char
+    output: prompt
+    return: int
+*/
 int main()
 {
     char encodedFileName[MAXLENGTH];
@@ -26,7 +48,7 @@ int main()
 
     welcome();
 
-    while (true)
+    while (tolower(menuOption) != 3)
     {
         // display menu
         displayMenu();
@@ -57,11 +79,25 @@ int main()
     return 0;
 }
 
+/*
+    Name:   welcome()
+    Desc:   welcomes the user
+    input:  none
+    output: none
+    return: void
+*/
 void welcome()
 {
     cout << "Welcome to my fileName encoding program!!\n\n";
 }
 
+/*
+    Name:   displayMenu()
+    Desc:   displays the menu
+    input:  none
+    output: none
+    return: void
+*/
 void displayMenu()
 {
     cout << "Please pick an option below:\n"
@@ -69,14 +105,26 @@ void displayMenu()
          << "(q) Quit\n";
 }
 
-// reads user menu input
+/*
+    Name:   readOption()
+    Desc:   reads character from user
+    input:  char
+    output: prompt
+    return: void
+*/
 void readOption(char &option)
 {
     cout << ">> ";
     cin.get(option);
 }
 
-// controls collection of all user input
+/*
+    Name:   encode()
+    Desc:   controller function for building encoded file name
+    input:  none
+    output: prompt, char
+    return: void
+*/
 void encode(char encodedFileName[])
 {
     char fName[NAMELENGTH],
@@ -129,9 +177,20 @@ void encode(char encodedFileName[])
          << endl;
 }
 
+/*
+    Name:   readInput()
+    Desc:   reads the first and last name from the user
+            itterates through each name to and makes
+                the characters lowercase
+            colletes late flag
+    input:  char[], bool
+    output: none
+    return: void
+*/
 void readInput(char fName[], char lName[], bool &lateFlag)
 {
     char tempFlag;
+    int length;
     bool loopState;
 
     // input last name
@@ -139,12 +198,21 @@ void readInput(char fName[], char lName[], bool &lateFlag)
          << "Enter your last name: ";
     cin.ignore();
     cin.get(lName, NAMELENGTH, '\n');
-
+    length = strlen(lName);
+    for (int i = 0; i < length; i++)
+    {
+        lName[i] = tolower(lName[i]);
+    }
     // input first name
     cout << '\n'
          << "Enter your first name: ";
     cin.ignore();
     cin.get(fName, NAMELENGTH, '\n');
+    length = strlen(fName);
+    for (int i = 0; i < length; i++)
+    {
+        fName[i] = tolower(fName[i]);
+    }
     cin.ignore();
 
     // is assignment late?
@@ -180,7 +248,14 @@ void readInput(char fName[], char lName[], bool &lateFlag)
     cout << endl;
 }
 
-// read ID and file name
+/*
+    Name:   readInput()
+    Desc:   reads ID and file name input from the user
+            performs error validation
+    input:  char
+    output: prompt
+    return: void
+*/
 void readInput(char parsedId[], char fileName[])
 {
     char tempId[12];
@@ -236,7 +311,15 @@ void readInput(char parsedId[], char fileName[])
     cout << endl;
 }
 
-// read time from user
+/*
+    Name:   readTime()
+    Desc:   reads time input from the user
+            performs validation on time input
+            reformats time if needed
+    input:  char
+    output: prompt
+    return: void
+*/
 void readTime(char strTime[])
 {
     // regex for checking if user input conforms to 00:00 - 23:59
@@ -249,6 +332,14 @@ void readTime(char strTime[])
         cout << "Enter the time submitted (military time - ex: 18:24 for 6:24pm): ";
         cin.get(strTime, TIME, '\n');
         cin.ignore();
+
+        if (strTime[1] == ':')
+        {
+            char temp[6];
+            sprintf(temp, "0%s", strTime);
+            strcpy(strTime, temp);
+        }
+
         cout << endl;
         // check user input, if format is corect set loopState to false
         if (regex_match(strTime, pattern))
